10e23c45a128cbb303da4982f060105773e74a15
Mont-alpha <cristianxlokl@gmail.com>
2025-11-29 16:47:34 -0300

    avance 5: descripcion del taller

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,8 @@ function Home() {
   return (
     <div>
       <Navbar />
-      home
+      <h1>Cristian Montiel</h1>
+      <p>El trabajo consiste en una aplicacion que consume la <a href="https://swapi.dev/">API de Star Wars</a> para mostrar peliculas, personajes, naves , planetas y entre otros datos relacionados con la saga.</p>
     </div>
   )
 }
